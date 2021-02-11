3e62f5b1725950e6e0a4dd0cd5a025d40d1dd65e
phyoethu100 <60525855+phyoethu100@users.noreply.github.com>
2021-02-11 00:39:01 -0800

    Update mfs.c

--- mfs.c
+++ mfs.c
@@ -1,17 +1,7 @@
 /**************************************************************************
-* Class:  CSC-415-01 Fall 2020
-* Team Name: A++ 
-* Student Name-ID: Roberto Herman (918009734), Cindy Fung Chan (920832364)
-*                  Phyoe Thu (918656575), Aryanna Brown (920188955)            
-* GitHub UserID: mecosteas, cny257, phyoethu100, aryannayazmin   
-* Project: Group term assignment - Basic File System 
-* 
-* File:  mfs.c
-*
 * Description: Functions for traversing through the file system, creating
 *              directories, modifying files, and displaying them. This
-*			   interacts directly with our driver.
-*              
+*			   interacts directly with our driver.        
 **************************************************************************/
 
 #include <stdlib.h>
