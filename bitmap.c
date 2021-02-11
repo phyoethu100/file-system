d9fffa9550188d64e0f3d0c7311d03239820385c
phyoethu100 <60525855+phyoethu100@users.noreply.github.com>
2021-02-11 00:41:44 -0800

    Update bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -1,17 +1,7 @@
 /**************************************************************************
-* Class:  CSC-415-01 Fall 2020
-* Team Name: A++ 
-* Student Name-ID: Roberto Herman (918009734), Cindy Fung Chan (920832364)
-*                  Phyoe Thu (918656575), Aryanna Brown (920188955)            
-* GitHub UserID: mecosteas, cny257, phyoethu100, aryannayazmin   
-* Project: Group term assignment - Basic File System 
-* 
-* File:  bitmap.c
-*
 * Description: Functions to find free space within our bitmap which
 *              represent the available LBA blocks to write to. Functions
-*              to mark spaces as occupied are also defined here.
-*              
+*              to mark spaces as occupied are also defined here.             
 **************************************************************************/
 
 // 0: free
